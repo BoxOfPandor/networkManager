d4567194a60240665c9fd34db5028c6f3e861447
Thibault Pouch <thibault.pouch@epitech.eu>
2024-07-08 14:25:50 +0200

    [refactor] change the "supprimerDevice" function

--- src/routes/device.query.js
+++ src/routes/device.query.js
@@ -10,9 +10,14 @@ function ajouterDevice(ip, mac, vendor, commentaire) {
 }
 
 // Fonction pour supprimer un élément
-function supprimerDevice(id) {
-    const sql = `DELETE FROM Devices WHERE Id = ?`;
-    db.query(sql, [id], (err, result) => {
+function supprimerDevice(ipOrMac) {
+    let sql;
+    if (ipOrMac.includes('.')) { // Supposition que c'est une adresse IP si elle contient un point
+        sql = `DELETE FROM Devices WHERE Ip = ?`;
+    } else { // Sinon, on suppose que c'est une adresse MAC
+        sql = `DELETE FROM Devices WHERE Mac = ?`;
+    }
+    db.query(sql, [ipOrMac], (err, result) => {
         if (err) throw err;
         console.log('Élément supprimé', result);
     });
