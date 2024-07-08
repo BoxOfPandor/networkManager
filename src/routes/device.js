fdcde9c91b5b0a1eeb37b070295b7900810f0317
Thibault Pouch <thibault.pouch@epitech.eu>
2024-07-08 14:24:38 +0200

    [feat] add the rout for the delete of a device

--- src/routes/device.js
+++ src/routes/device.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-// Importer scanNetwork depuis func/networkScan.js
 const { scanNetwork } = require('../func/networkScan.js');
+const { supprimerDevice, ajouterDevice } = require('./device.query.js');
 
-// Fonction de comparaison pour trier les adresses IP
 function compareIPs(a, b) {
     const ipA = a.ip.split('.').map(Number);
     const ipB = b.ip.split('.').map(Number);
@@ -16,7 +15,6 @@ function compareIPs(a, b) {
 }
 
 router.post('/device', (req, res) => {
-    // Récupérer l'adresse IP ou l'adresse MAC depuis les paramètres de la requête
     const { ip, mac } = req.query;
 
     if (!ip && !mac) {
@@ -24,10 +22,10 @@ router.post('/device', (req, res) => {
     }
 
     scanNetwork().then(devices => {
+        devices = devices || []; // Ensure devices is an array
         const deviceFound = devices.find(device => device.ip === ip || device.mac === mac);
 
         if (deviceFound) {
-            const { ajouterDevice } = require('./device.query.js');
             ajouterDevice(deviceFound.ip, deviceFound.mac, deviceFound.vendor, "Ajouté automatiquement");
             res.send({ message: "L'élément a été ajouté avec succès." });
         } else {
@@ -38,3 +36,28 @@ router.post('/device', (req, res) => {
         res.status(500).send({ message: "Erreur lors de la recherche dans le réseau." });
     });
 });
+
+router.delete('/device', (req, res) => {
+    const { ip, mac } = req.query;
+
+    if ((!ip || ip.trim() === "") && (!mac || mac.trim() === "")) {
+        return res.status(400).send({ message: "Veuillez fournir une adresse IP ou une adresse MAC valide." });
+    }
+
+    scanNetwork().then(devices => {
+        devices = devices || []; // Ensure devices is an array
+        const deviceFound = devices.find(device => device.ip === ip || device.mac === mac);
+
+        if (deviceFound) {
+            supprimerDevice(deviceFound.ip || deviceFound.mac);
+            res.send({ message: "L'élément a été supprimé avec succès." });
+        } else {
+            res.status(404).send({ message: "L'élément n'a pas été trouvé dans le réseau." });
+        }
+    }).catch(error => {
+        console.error("Erreur lors de la recherche dans le réseau :", error);
+        res.status(500).send({ message: "Erreur lors de la recherche dans le réseau." });
+    });
+});
+
+module.exports = router;
